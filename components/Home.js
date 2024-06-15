7cdc6390d9b426890e87d243efd564377ab36b72
FMZIN <rafasandei17@gmail.com>
2024-06-15 13:38:43 -0300

    Part2

--- components/Home.js
+++ components/Home.js
@@ -1,20 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import { Image } from 'expo-image';
 
 import perfil from '../assets/perfil.jpg';
+import fundo from '../assets/fundo.jpg';
 
 export default function Home() {
   return (
-    <View style={styles.container}>
-      <Image
-        source={perfil}
-        style={styles.perfil}
-      />
-      <View style={styles.descricao2}>
-        <Text style={styles.nome}>Yuki Tsunoda</Text>
-        <Text style={styles.descricao}>Yuki Tsunoda é um piloto japonês de corridas de automóveis, nascido em 11 de maio de 2000, conhecido por competir na Fórmula 1. Ele estreou na Fórmula 1 na temporada de 2021, competindo pela equipe AlphaTauri.</Text>
+    // <ImageBackground source={fundo} style={styles.fundo}>
+      <View style={styles.container}>
+        <Image
+          source={perfil}
+          style={styles.perfil}
+        />
+        <View style={styles.descricao2}>
+          <Text style={styles.nome}>Yuki Tsunoda</Text>
+          <Text style={styles.descricao}>Yuki Tsunoda é um piloto japonês de corridas de automóveis, nascido em 11 de maio de 2000, conhecido por competir na Fórmula 1. Ele estreou na Fórmula 1 na temporada de 2021, competindo pela equipe AlphaTauri.</Text>
+        </View>
       </View>
-    </View>
+    // </ImageBackground>
   );
 }
 
@@ -24,6 +27,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  fundo:{
+    width: 250,
+    height: 250,
   },
   perfil:{
     width: 250,
@@ -43,6 +50,7 @@ const styles = StyleSheet.create({
     backgroundColor: "grey",
     marginLeft: 20,
     marginRight: 20,
-    marginTop: 30
+    marginTop: 30,
+    borderRadius: 10
   },
 });
--- components/Sobre.js
+++ components/Sobre.js
@@ -1,11 +1,64 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, ScrollView } from 'react-native';
 import { Image } from 'expo-image';
+
+import perfil from '../assets/perfil.jpg';
+import foto1 from '../assets/foto1.jpg';
+import foto2 from '../assets/foto2.jpeg';
+import foto3 from '../assets/foto3.webp';
+import foto4 from '../assets/foto4.png';
 
 export default function Sobre() {
   return (
-    <View style={styles.container}>
-      <Text>Pagina Sobre</Text>
-    </View>
+    <ScrollView>    
+      <View style={styles.container}>
+        <Text style={styles.nome}>Yuki Tsunoda</Text>
+        <View style={styles.container2}>
+          <Image source={perfil} style={styles.foto}/>
+          <Text style={styles.descricao}>Yuki Tsunoda, nascido em 2000 no Japão, iniciou sua carreira no karting em 2010. Ele se destacou na Fórmula 4 Japonesa, terminando em terceiro em 2017. Após competir na Fórmula 3 em 2019 e na Fórmula 2 em 2020, onde também ficou em terceiro, Tsunoda estreou na Fórmula 1 pela Scuderia AlphaTauri em 2021. Ele mostrou talento e velocidade, enfrentando alguns desafios típicos de um novato.</Text>
+        </View>
+
+        <View style={styles.container3}>
+          <Image source={foto1} style={styles.foto1}/>
+          <View>
+            <Text style={styles.altura}>Altura</Text>
+          </View>
+          <View>
+            <Text style={styles.altura2}>Com 1,59 m de altura, Tsunoda é um dos pilotos mais baixos da Fórmula 1 moderna.</Text>
+          </View>
+        </View>
+
+        <View style={styles.container3}>
+          <Image source={foto2} style={styles.foto1}/>
+          <View>
+            <Text style={styles.altura}>Treinamento Diferenciado</Text>
+          </View>
+          <View>
+            <Text style={styles.altura2}>Como muitos jovens de sua geração, Tsunoda gosta de videogames e já mencionou que jogar simuladores de corrida ajudou em seu treinamento.</Text>
+          </View>
+        </View>
+
+        <View style={styles.container3}>
+          <Image source={foto3} style={styles.foto1}/>
+          <View>
+            <Text style={styles.altura}>Gosto pela Culinária</Text>
+          </View>
+          <View>
+            <Text style={styles.altura2}>Yuki gosta de cozinhar e frequentemente compartilha fotos de suas refeições caseiras em suas redes sociais.</Text>
+          </View>
+        </View>
+
+        <View style={styles.container3}>
+          <Image source={foto4} style={styles.foto1}/>
+          <View>
+            <Text style={styles.altura}>Queridinho da Honda</Text>
+          </View>
+          <View>
+            <Text style={styles.altura2}>Tsunoda foi fortemente apoiado pela Honda, sendo parte de seu programa de desenvolvimento de pilotos desde cedo, o que facilitou sua entrada na Fórmula 1.</Text>
+          </View>
+        </View>
+
+      </View>
+    </ScrollView>
   );
 }
 
@@ -13,7 +66,43 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  container2: {
+    alignItems: "center"
+  },
+  container3: {
+    marginTop: 20,
+    margin: 10,
+    flexDirection: "row",
+  },
+  nome: {
+    fontSize: 20,
+    marginTop: 10,
+    marginBottom: 10,
+    fontWeight: "bold",
+    alignItems: "flex-start",
+    marginLeft: 40
+  },
+  foto: {
+    width: 400,
+    height: 230
+  },
+  descricao: {
+    marginLeft: 42,
+    marginRight: 42,
+    marginTop: 10,
+    marginBottom: 10
+  },
+  foto1: {
+    width: 100,
+    height: 100
+  },
+  altura: {
+    marginLeft: 10,
+    fontWeight: "bold",
+    fontSize: 15
+  },
+  altura2: {
+    marginLeft: 10
+  }
 });
